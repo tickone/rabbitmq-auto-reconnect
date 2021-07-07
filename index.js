ed66f25e76f2ee0c93fb88920269533d282945f4
Michael Kuan <michael34435@capslock.tw>
2021-07-08 02:47:43 +0800

    chore: 1.1.1

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ const debug = require('debug')('rabbitmq');
 
 class RabbitMQ {
   constructor(host) {
-    // record start time, skip message before start
     this.startTime = new Date().getTime();
     this.host = host;
-    /** @type {import('amqplib/callback_api').Connection} */
     this.connection = null;
-    /** @type {import('amqplib/callback_api').Channel} */
-    this.channel = null;
+    this.channel = {};
   }
 
   async connect() {
     return new Promise((resolve, reject) => {
-      amqp.connect(this.host, {
-        timeout: 5000,
-      }, (error, connection) => {
+      amqp.connect(this.host, 'heartbeat=30', (error, connection) => {
         if (error) {
           reject(error);
         } else {
+          this.channel = {};
           this.connection = connection;
           this.connection.on('error', (connectionError) => {
             debug('[AMQP] error', { error: connectionError });
@@ -35,16 +31,22 @@ class RabbitMQ {
     });
   }
 
-  async createChannel() {
+  async createChannel(type, queueName) {
     if (!this.connection) {
       await this.connect();
     }
+
+    if (this.channel?.[type]?.[queueName]) {
+      return this.channel?.[type]?.[queueName];
+    }
+
     return new Promise((resolve, reject) => {
       this.connection.createChannel((error, channel) => {
         if (error) {
           reject(error);
         } else {
-          this.channel = channel;
+          this.channel[type] = this.channel[type] ?? {};
+          this.channel[type][queueName] = channel;
           resolve(channel);
           debug('[AMQP] rabbitmq channel created');
         }
@@ -52,39 +54,21 @@ class RabbitMQ {
     });
   }
 
-  async recoverChannel() {
+  async recoverChannel(type, queueName) {
     return new Promise((resolve) => {
       // this.channel.nackAll(false)
-      this.channel.recover(resolve);
+      this.channel?.[type]?.[queueName].recover(resolve);
     });
   }
 
   /**
    * @returns {Promise<import('amqplib/callback_api').Channel>}
    */
-  async getChannel() {
-    if (!this.channel) {
-      await this.createChannel();
-      await this.recoverChannel();
-    }
-    return this.channel;
-  }
+  async getChannel(type, queueName) {
+    const channel = await this.createChannel(type, queueName);
+    await this.recoverChannel(type, queueName);
 
-  async purgeQueue(queueName) {
-    const channel = await this.getChannel();
-    return new Promise((resolve, reject) => {
-      channel.purgeQueue(queueName, (error, ok) => {
-        if (error) {
-          debug(`[AMQP/${queueName}] purgeQueue error`, { error });
-          reject(error);
-          return;
-        }
-        if (ok) {
-          debug(`[AMQP/${queueName}] purgeQueue ok`, { ok });
-          resolve();
-        }
-      });
-    });
+    return channel;
   }
 
   /**
@@ -100,17 +84,13 @@ class RabbitMQ {
     sendOptions = { persistent: true },
   ) {
     try {
-      const channel = await this.getChannel();
+      const channel = await this.getChannel('send', queueName);
 
-      const payload = {
-        ...message,
-        time: new Date().getTime(),
-      };
       channel.assertQueue(queueName, queueOptions);
-      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(payload)), sendOptions);
-      debug(`[AMQP/${queueName}/${message.type}] sended`, { message });
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), sendOptions);
+      debug(`[AMQP/${queueName}] sended`, { message });
     } catch (error) {
-      debug(`[AMQP/${queueName}/${message.type}] sendToQueue error`, { error });
+      debug(`[AMQP/${queueName}] sendToQueue error`, { error });
     }
   }
 
@@ -127,27 +107,18 @@ class RabbitMQ {
     queueOptions = { durable: true },
     consumeOptions = { noAck: false },
   ) {
-    const channel = await this.getChannel();
+    const channel = await this.getChannel('listen', queueName);
     channel.assertQueue(queueName, queueOptions);
     channel.prefetch(prefetchCount);
-
-    await this.purgeQueue(queueName);
 
     channel.consume(queueName, async (msg) => {
       try {
         /** @type {Message} */
         const message = JSON.parse(msg.content.toString());
-        debug(`[AMQP/${queueName}/${message.type}] onMessage`, { message });
-
-        if (message.time < this.startTime) {
-          // skip message before start time
-          debug(`[AMQP/${queueName}/${message.type}] nack (skip old message)`);
-          channel.nack(msg, false, false);
-          return;
-        }
+        debug(`[AMQP/${queueName}] onMessage`, { message });
 
         await callback(message);
-        debug(`[AMQP/${queueName}/${message.type}] ack (${message.type})`);
+        debug(`[AMQP/${queueName}] ack (${message.type})`);
         channel.ack(msg);
       } catch (error) {
         debug(`[AMQP/${queueName}] nack`, { error });
